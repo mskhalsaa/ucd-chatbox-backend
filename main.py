028e18e9938677774bf4298f8fb379df14527ad0
mskhalsaa <mskhalsa@ucdavis.edu>
2026-02-09 18:38:37 -0800

    Update chat endpoint and API routes

--- main.py
+++ main.py
@@ -1,65 +1,45 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict, List
+from typing import Optional
 import uuid
+
 
 
 app = FastAPI()
 
-#defines the shape of the incoming request
+
 class ChatRequest(BaseModel):
     message: str
-    conversation_id: str | None = None
+    conversation_id: Optional[str] = None
+
 
 class ChatResponse(BaseModel):
     conversation_id: str
     response: str
 
 
-conversations: Dict[str, List[Dict[str, str]]] = {}
-
-
-
-
-
-#this is the chat service
 @app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
-    # create new conversation if needed
+    # create conversation id if frontend doesn't send one
+    conversation_id = request.conversation_id or str(uuid.uuid4())
 
-    if request.conversation_id is None:
-        conversation_id = str(uuid.uuid4())
-        conversations[conversation_id] =[]
-    else:
-        conversation_id = request.conversation_id
-        conversations.setdefault(conversation_id, [])
+    # temporary fake response
+    fake_response = f"You said: '{request.message}'. UC Davis chatbot coming soon."
 
-    # save user message
+    return ChatResponse(
+        conversation_id=conversation_id,
+        response=fake_response
+    )
 
-    conversations[conversation_id].append( {
-        "role": "user",
-        "content": request.message
-    })
 
-    # fake ai response
-    ai_response = f"(AI) I received: {request.message}"
 
-    #save AI response
-    conversations[conversation_id].append({
-         "role": "assistant",
-        "content": ai_response
-    })
 
-    return {
-        "conversation_id": conversation_id,
-        "response": ai_response
-    }
-@app.get("/conversations/{conversation_id}")
-def get_conversation(conversation_id: str):
-    return {
-        "conversation_id": conversation_id,
-        "messages": conversations.get(conversation_id, [])
-    }
+
+
+
+
+
     
 
 
